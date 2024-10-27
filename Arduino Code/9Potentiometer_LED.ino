e61c65f905d64fa2d6f86aba4aee57022efdc977
MohammadamMan <mohammadzadhe379@gmail.com>
2024-10-27 21:43:29 +0330

    Create 9Potentiometer_LED.ino

---
+++ Arduino Code/9Potentiometer_LED.ino
@@ -0,0 +1,17 @@
+
+int ledpin=6;
+int val=0;
+
+void setup() {
+pinMode(ledpin, OUTPUT);
+pinMode(A0,INPUT);
+}
+
+void loop() {
+ val=analogRead(A0);
+ int newval= map(val,0,1023,0,255);
+ analogWrite(ledpin, newval);
+
+}
+
+//mohammadzadeh
